1e059bee5240567da9704b4d3d0c745f7c5ab897
LeCauchy <godsendmelie@gmail.com>
2022-11-22 17:34:08 +0100

    modification of zabbix-ipsec.py

--- scripts/zabbix-ipsec.py
+++ scripts/zabbix-ipsec.py
@@ -1,46 +1,13 @@
-#!/usr/local/bin/python3.7
+#!/usr/local/bin/python3.8
 
-import itertools
 import re
 import sys
 import xml.etree.cElementTree as ET
 
-IPSEC_CONF = '/var/etc/ipsec/swanctl.conf'
-PFSENSE_CONF = '/conf/config.xml'
+IPSEC_CONF = './swanctl.conf'
 rtt_time_warn = 200
 rtt_time_error = 300
 
-#Parse the XML
-tree = ET.parse(PFSENSE_CONF)
-root = tree.getroot()
-
-#Function to find phase description by ikeid
-def findDescr(remoteid,ikeid):
-
-        #Check if the parameter was sent
-        if not remoteid:
-                return "Not found"
-
-        #create search string. We use the "..." after the search to return the parent element of the current element.
-        #The reason for that is the remoteid is a sub element of phase2 element 
-        search = "./ipsec/phase2/remoteid/[address='" + remoteid + "']..."
-
-        for tunnel in root.findall(search):
-                descr = tunnel.find('descr').text
-
-                #If we have only one result, we are talking about the correct tunnel
-                if len(root.findall(search)) == 1:
-                        return descr
-
-                #otherwise, if we have more than 1, we have to confirm the remoteid and the ikeid 
-                #Case the ikeIds are the same, we got it. Case not, we pass and wait for next interation
-                else:
-                        #Get the ikeid of this element
-                        ikeidElement = tunnel.find('ikeid').text
-                        if ikeidElement == ikeid:
-                                return descr
-
-        return "Not found"
 
 #Function to set correct format on ikeId. Recives conIDXXX, return ID
 def formatIkeId(ikeid):
@@ -58,37 +25,45 @@ def formatIkeId(ikeid):
     return ikeid
 
 def parseConf():
-    reg_conn = re.compile('^\s*con[0-9]{4,6}')
-    reg_left = re.compile('.*local_addrs =(.*).*')
-    reg_right = re.compile('.*remote_addrs =(.*).*')
-    reg_rightsubnet = re.compile('.*remote_ts =(.*).*')
+    reg_conn = re.compile('^\s*con[0-9]*')
+    reg_left = re.compile('local_addrs = .*')
+    reg_right = re.compile('remote_addrs = .*')
+    reg_descr = re.compile('\t\t.*ikeid.*')
+    # reg_rightsubnet = re.compile('.*remote_ts =(.*).*')
     data = {}
     with open(IPSEC_CONF, 'r') as f:
         soubor = f.read()
-        groups = re.findall('(^\s*con[0-9]+.*?)(?=^\s*esp_proposals|\Z)', soubor, flags=re.DOTALL|re.MULTILINE)
+        groups = re.findall('(^\s*con[0-9]+ \{.*?)(?=^\s*esp_proposals|\Z)', soubor, flags=re.DOTALL|re.MULTILINE)
         for g in groups:
             conn_tmp = list()
             m = re.search(reg_conn, g)
-            m = m.group(0)
+            m = m.group()
             m = m.lstrip('\t')
             m = m.replace('\n\t','')
             if m:
                 conn_tmp.append(m)
             left_tmp = list()
             m1 = re.search(reg_left, g)
-            m1 = m1.group(0)
+            m1 = m1.group()
             m1 = m1.strip('\t\tlocal_addrs =')
             if m1:
                 left_tmp.append(m1)
             right_tmp = list()
             m2 = re.search(reg_right, g)
-            m2 = m2.group(0)
+            m2 = m2.group()
             m2 = m2.strip('\t\tremote_addrs =')
             if m2:
                 right_tmp.append(m2)
             descr = "Not found"
-            if conn_tmp and left_tmp and right_tmp:
-                    data[conn_tmp[0]] = [left_tmp[0], right_tmp[0], descr]
+            descr_tmp = list()
+            m3 = re.search(reg_descr, g)
+            m3 = m3.group()
+            m3 = m3.strip('\t\t# .*:')
+            if m3:
+                descr_tmp.append(m3)
+            if conn_tmp and left_tmp and right_tmp and descr_tmp:
+                    data[conn_tmp[0]] = [left_tmp[0], right_tmp[0], descr_tmp[0]]
+
         return data
 
 def getTemplate():
